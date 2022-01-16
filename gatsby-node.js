d99f0debabcc118d29ed05269d0f44d7e7d46e41
FinnRG <finngaertner@protonmail.com>
2022-01-16 13:51:19 +0100

    feat: Migrate to gatsby-plugin-mdx

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ const { createFilePath } = require("gatsby-source-filesystem")
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
 
-  if (node.internal.type === "MarkdownRemark") {
+  if (node.internal.type === "Mdx") {
     const slug = createFilePath({ node, getNode, basePath: "pages" })
     createNodeField({ node, name: "slug", value: slug })
   }
@@ -15,7 +15,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
   const result = await graphql(`
     {
-      allMarkdownRemark {
+      allMdx {
         totalCount
         edges {
           node {
@@ -28,7 +28,7 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  result.data.allMdx.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
       component: path.resolve("./src/templates/Page.tsx"),
