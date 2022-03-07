93849dbdf0b88411aee51b629c1f463c6fd02bdf
Finn R. Gärtner <finngaertner@protonmail.com>
2022-03-07 23:13:22 +0100

    perf: Optimize bg and use preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-preact",
     {
       resolve: "gatsby-source-contentful",
       options: {
