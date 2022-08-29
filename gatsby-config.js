e66ce8c17769e91ae21964164ef7d18510ea6f63
FinnRG <finngaertner@protonmail.com>
2022-08-29 22:13:17 +0200

    fix: DIsable gatsby-plugin-preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    "gatsby-plugin-preact",
     {
       resolve: "gatsby-source-contentful",
       options: {
